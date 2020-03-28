a35d6f5eb383701e907f20dc0a019905b28c4f33
miyamo93 <miyamo0802@gmail.com>
2020-03-28 17:47:22 +0900

    クイズゲーム練習

--- js/main03.js
+++ js/main03.js
@@ -4,14 +4,17 @@
   const question = document.getElementById('question');
   const choices = document.getElementById('choices');
   const qbtn = document.getElementById('qbtn');
+  const result = document.getElementById('result');
+  const scoreLabel = document.querySelector('#result > p');
 
-  const quizSet = [
-    {q: 'Whoat is A?', c:['A0' ,'A1' ,'A2' ]},
-    {q: 'Whoat is B?', c:['B0' ,'B1' ,'B2' ]},
-    {q: 'Whoat is C?', c:['C0' ,'C1' ,'C2' ]}
-  ]
+  const quizSet =  shuffle( [
+    {q: '世界で一番大きい湖は', c:['カスピ海' ,'カリブ海' ,'琵琶湖' ]},
+    {q: '2の8乗は', c:['256' ,'64' ,'1024' ]},
+    {q: '次のうち、最初にリリースされた言語は？', c:['Python' ,'JavaScript' ,'HTML' ]}
+  ]);
   let currentNum = 0;
-  
+  let isAnswered;
+  let score = 0;
   
 
   function shuffle(arr){
@@ -24,19 +27,33 @@
   }
 
   function checkAnswer(li){
-    if (li.textContent === quizSet[currentNum].c[0]){
-      console.log('correct');
-    } else{
-      console.log('wrong');
+    if (isAnswered === true){
+      return;
     }
+    isAnswered = true;
+    if (li.textContent === quizSet[currentNum].c[0]){
+      li.classList.add('correct');
+      score++;
+    } else{
+      li.classList.add('wrong');
+    }
+    qbtn.classList.remove('disabled')
   }
   
 
   function setQuiz(){
+    isAnswered = false;
+
+
     question.textContent = quizSet[currentNum].q;
 
+    while(choices.firstChild){
+      choices.removeChild(choices.firstChild);
+    }
+
+    
     const shuffledChoices = shuffle([...quizSet[currentNum].c]);
-    console.log(quizSet[currentNum].c);
+    
     shuffledChoices.forEach(choice => {
       const li = document.createElement('li');
       li.textContent = choice;
@@ -45,6 +62,27 @@
       });
       choices.appendChild(li)
     });
+
+    if(currentNum ===  quizSet.length - 1){
+      qbtn.textContent = 'show Score';
+    }
   }
+
   setQuiz();
+
+  qbtn.addEventListener('click' ,() => {
+    if (qbtn.classList.contains('disabled')){
+      return;
+    }
+    qbtn.classList.add('disabled');
+
+    if (currentNum === quizSet.length - 1){
+      // console.log(`Score: ${score} /  ${quizSet.length}`);
+      scoreLabel.textContent = `Score: ${score} /  ${quizSet.length}`;
+      result.classList.remove('hidden');
+    } else{
+      currentNum++;
+      setQuiz();
+    }
+  });
 }
